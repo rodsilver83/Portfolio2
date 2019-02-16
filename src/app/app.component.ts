1a617030efb990a89806b73bed899e699445b378
Jose Soria <josesoria@Globants-MacBook-Pro.local>
2019-02-15 22:45:27 -0500

    add circles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { compileNgModule } from '@angular/core/src/render3/jit/module';
 
-
-const MAX_VEL = 0.5;
-const MIN_VEL = 0.21;
-const MAX_DISTANCE = 250;//px
-const WINDOW_OFFSET = 50;//px
-const CONST_VY = -0.4;
-const MAX_CONST_VX = -0.0;
-const DOT_COLOR = "#2A4F6E";
-const START_GRADIENT = "#042037";
-const END_GRADIENT = "#003A23";
-const INIT_SIZE_DOT = 0;
-const DOT_GROW_SIZE = 0;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -27,6 +14,21 @@ export class AppComponent implements AfterViewInit {
   public dots: number;
   public CONST_VX = -0.0;
   public T = 0;
+  public MAX_VEL = 0.5;
+  public MIN_VEL = 0.21;
+  public MAX_DISTANCE = 250;//px
+  public WINDOW_OFFSET = 50;//px
+  public CONST_VY = -0.4;
+  public MAX_CONST_VX = -0.0;
+  public DOT_COLOR = "#2A4F6E";
+  public START_GRADIENT = "#042037";
+  public END_GRADIENT = "#003A23";
+  public INIT_SIZE_DOT = 0;
+  public DOT_GROW_SIZE = 0.05;
+  public DOT_MAX_SIZE = 150;
+  public FILL_DOT = false;
+
+  private interval: any;
 
   ngAfterViewInit() {
     this.canvas = document.getElementById("canvas-element");
@@ -34,11 +36,18 @@ export class AppComponent implements AfterViewInit {
     this.context = this.canvas.getContext("2d");
     
     this.particles = [];
-    this.dots = Math.floor(window.innerWidth/20);
+    this.dots = Math.floor(window.innerWidth/40);
         
     // if(window.innerWidth >= 992){
       this.initCanvas();
     // }
+  }
+
+  apply() {
+    window.clearInterval(this.interval);
+    this.particles = [];
+    this.dots = Math.floor(window.innerWidth/20);
+    this.initCanvas();
   }
 
   speedParticlesX() {
@@ -48,7 +57,7 @@ export class AppComponent implements AfterViewInit {
       // this.clearInterval(goAboutInterval);
       this.CONST_VX = 0;
     }else{
-      this.CONST_VX = ease * MAX_CONST_VX;
+      this.CONST_VX = ease * this.MAX_CONST_VX;
     }
   }
   /**
@@ -78,18 +87,21 @@ export class AppComponent implements AfterViewInit {
         y: Math.random() * window.innerHeight,
         vx: this.randomVelX(i),
         vy: this.randomVelY(i),
-        size: INIT_SIZE_DOT,
-        color: DOT_COLOR,
+        size: this.INIT_SIZE_DOT,
+        color: this.DOT_COLOR,
+        alpha: 0,
         id: 0
       });
     }
 
-    setInterval(() => {
+    this.interval = setInterval(() => {
       if(this.context) {
         this.context.clearRect(0, 0, window.innerWidth, window.innerHeight);
         this.fillCanvasGradient();
         for( let i = 0; i < this.particles.length; i++ ) {
-          this.particles[i].size = INIT_SIZE_DOT;
+          if(this.particles[i].size > this.DOT_MAX_SIZE) {
+            this.particles[i].size = this.INIT_SIZE_DOT;
+          }
           this.particles[i].id = i;
           this.moveParticles(i);
           this.calculateLines(i);
@@ -101,20 +113,20 @@ export class AppComponent implements AfterViewInit {
 
   randomVelX(i){
     if( i % 4 == 0){
-      return -Math.random()*MAX_VEL-MIN_VEL;
+      return -Math.random() * this.MAX_VEL - this.MIN_VEL;
     }
     if( i % 4 == 1){
-      return Math.random()*MAX_VEL+MIN_VEL;
+      return Math.random() * this.MAX_VEL + this.MIN_VEL;
     }
     return 0.0;
   }
 
   randomVelY(i){
     if( i % 4 == 2){
-      return -Math.random()*MAX_VEL-MIN_VEL;
+      return -Math.random() * this.MAX_VEL - this.MIN_VEL;
     }
     if( i % 4 == 3){
-      return Math.random()*MAX_VEL+MIN_VEL;
+      return Math.random() * this.MAX_VEL + this.MIN_VEL;
     }
     return 0.0;
   }
@@ -122,8 +134,8 @@ export class AppComponent implements AfterViewInit {
   fillCanvasGradient(){
     // Create gradient
     let grd = this.context.createLinearGradient(0,0,0,window.innerHeight);
-    grd.addColorStop(0,START_GRADIENT);
-    grd.addColorStop(1,END_GRADIENT);
+    grd.addColorStop(0,this.START_GRADIENT);
+    grd.addColorStop(1,this.END_GRADIENT);
 
     // Fill with gradient
     this.context.fillStyle = grd;
@@ -133,29 +145,39 @@ export class AppComponent implements AfterViewInit {
   calculateLines(i){
     for(let j=i + 1; j < this.particles.length; j++){
       var distance = this.distanceBetweenDots(this.particles[i],this.particles[j]);
-      if(distance < MAX_DISTANCE){
-        this.particles[i].size += DOT_GROW_SIZE;
-        this.particles[j].size += DOT_GROW_SIZE;
-        this.drawLine(this.particles[i],this.particles[j], distance);
+      if(distance < this.MAX_DISTANCE){
+        this.particles[i].size += this.DOT_GROW_SIZE;
+        this.particles[j].size += this.DOT_GROW_SIZE;
+        this.particles[i].alpha = this.calculateAlpha(distance);
+        this.drawLine(this.particles[i],this.particles[j]);
       }
     }
   }
 
-  drawLine(p1,p2, distance) {
-    //context.strokeStyle = DOT_COLOR;
-    //0.164 0.309 0.431 1
-    var alpha = 1 - ( distance / MAX_DISTANCE);
+  hexToRGBA(hex, opacity) {
+    return 'rgba(' + (hex = hex.replace('#', '')).match(new RegExp('(.{' + hex.length/3 + '})', 'g')).map(function(l) { return parseInt(hex.length%2 ? l+l : l, 16) }).concat(opacity||1).join(',') + ')';
+  }
+
+  calculateAlpha(distance) {
+    let alpha = 1.1 - ( distance / this.MAX_DISTANCE);
     if(alpha > 1) {
       alpha = 1;
     }
     if(alpha < 0) {
       alpha = 0;
     }
+    return alpha;
+  }
 
+  drawLine(p1,p2) {
+    // this.context.strokeStyle = this.DOT_COLOR;
+
+    this.context.beginPath();
     this.context.moveTo(p1.x,p1.y);
-    this.context.strokeStyle = "rgba(42, 79, 110, "+alpha+")";
+    this.context.strokeStyle = this.hexToRGBA(this.DOT_COLOR, p1.alpha);
     this.context.lineTo(p2.x,p2.y);
     this.context.stroke();
+    this.context.closePath();
 
     //Test Alpha
     //testLineAlpha(p1,p2,alpha);
@@ -184,36 +206,36 @@ export class AppComponent implements AfterViewInit {
   }
 
   moveParticles(i){
-    this.particles[i].y += this.particles[i].vy + CONST_VY;
+    this.particles[i].y += this.particles[i].vy + this.CONST_VY;
     this.particles[i].x += this.particles[i].vx + this.CONST_VX;
 
-    if(this.particles[i].x > window.innerWidth + WINDOW_OFFSET){
-      this.particles[i].x = - WINDOW_OFFSET;
+    if(this.particles[i].x > window.innerWidth + this.WINDOW_OFFSET){
+      this.particles[i].x = - this.WINDOW_OFFSET;
     }
 
-    if(this.particles[i].x < -WINDOW_OFFSET){
-      this.particles[i].x = window.innerWidth + WINDOW_OFFSET;
+    if(this.particles[i].x < -this.WINDOW_OFFSET){
+      this.particles[i].x = window.innerWidth + this.WINDOW_OFFSET;
     }
 
-    if(this.particles[i].y > window.innerHeight + WINDOW_OFFSET){
-      this.particles[i].y = - WINDOW_OFFSET;
+    if(this.particles[i].y > window.innerHeight + this.WINDOW_OFFSET){
+      this.particles[i].y = - this.WINDOW_OFFSET;
     }
 
-    if(this.particles[i].y < - WINDOW_OFFSET){
-      this.particles[i].y = window.innerHeight + WINDOW_OFFSET;
+    if(this.particles[i].y < - this.WINDOW_OFFSET){
+      this.particles[i].y = window.innerHeight + this.WINDOW_OFFSET;
     }
   }
 
   drawDot(dot) {
-    this.context.fillStyle = dot.color;
+    let alpha = 1.1 - ( (dot.size) / this.DOT_MAX_SIZE);
+    this.context.strokeStyle = this.hexToRGBA(this.DOT_COLOR, alpha);
     this.context.beginPath();
     this.context.arc(dot.x, dot.y, dot.size, 0, 2 * Math.PI);
+    this.context.stroke();
     this.context.closePath();
-    this.context.fill();
-
-    //Test Text
-    //context.font = "15px Arial";
-    //context.fillText(dot.id,dot.x+5,dot.y-5);
+    if(this.FILL_DOT) {
+      this.context.fill();
+    }
   }
 
   resizeCanvas() {
